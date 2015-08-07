890ebb5f3a2d6a4c51b2598f4c2fbe453d5e4e18
eValam <hi_eva@qq.com>
2015-08-07 22:19:33 +0800

    Create example_from_eva.h

---
+++ Iteraction 1 meeting minutes and doc/example_from_eva.h
@@ -0,0 +1,28 @@
+# Benjamin Franklin  (本杰明·富兰克林)
+
+           rv.activities = [NSMutableArray arrayWithObjects: @"22", @"5", @"sleeping", @"0",
+                                                             @"5", @"8", @"think about what to do today", @"1",
+                                                             @"8", @"12", @"working", @"2",
+                                                             @"12", @"14", @"reading,check account book,lunch", @"1",
+                                                             @"14", @"18",@"working", @"2",
+                                                             @"18", @"20", @"supper,music,chat,entertainment", @"3",
+                                                             @"20", @"22", @"think about what did well today", @"1",nil];
+
+        rv.title = @"Benjamin Franklin";
+        rv.subTitle = @"C.1771";
+
+
+
+# Eva Lam  
+
+           rv.activities = [NSMutableArray arrayWithObjects: @"0.5", @"8", @"sleeping", @"0",
+                                                             @"8", @"9", @"washing and take a metro to office", @"3",
+                                                             @"9", @"11.5", @"working", @"2",
+                                                             @"11.5", @"13.5", @"lunch and break", @"1",
+                                                             @"13.5", @"18",@"working", @"2",
+                                                             @"18", @"19", @"supper,chat", @"1",
+                                                             @"19", @"21", @"'study nite' time", @"4",
+                                                             @"21", @"0.5", @"'watch TV,wechat,reading", @"3",nil];
+
+        rv.title = @"Eva Lam";
+        rv.subTitle = @"C.2015";
